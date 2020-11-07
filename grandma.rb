f3b4756aa23b8795435045f00670bc4eeff5ecd1
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-07 01:19:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    response = "I LOVE YOU TOO PUMPKIN"
+    response = "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     response = "NO, NOT SINCE 1938!"
   else response = "HUH?! SPEAK UP, SONNY!"
@@ -21,4 +21,3 @@ def speak_to_grandma(phrase)
   puts response
   response
 end
-  
